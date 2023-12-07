9a76f914b5faa122fc1af1241ae7d16db6bcef7e
tataadin <tataa2237@gmail.com>
2023-12-08 00:03:32 +0700

    [feature][main] register and implement new route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:monumen_submission/navigations_learn/navigation_learn.dart';
+
+import 'navigations_learn/another_screen.dart';
+import 'navigations_learn/navigation_learn.dart';
+import 'navigations_learn/replacement_screen.dart';
+import 'navigations_learn/return_data_screen.dart';
+import 'navigations_learn/second_screen.dart';
+import 'navigations_learn/second_screen_with_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,8 +21,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const NavigationLearn(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const NavigationLearn(),
+        '/secondScreen': (context) => const SecondScreen(),
+        '/secondScreenWithData': (context) => SecondScreenWithData(
+            ModalRoute.of(context)?.settings.arguments as String),
+        '/returnDataScreen': (context) => const ReturnDataScreen(),
+        '/replacementScreen': (context) => const ReplacementScreen(),
+        '/anotherScreen': (context) => const AnotherScreen(),
+      },
     );
   }
 }
