3b0d18520fda46fda59d9b0ffbeb231a03688003
tataadin <tataa2237@gmail.com>
2023-12-08 00:46:22 +0700

    [feature][nav_learn.replacement_screen] add actionbar and show string argument from another screen

--- lib/navigations_learn/second_screen_with_data.dart
+++ lib/navigations_learn/second_screen_with_data.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 class SecondScreenWithData extends StatelessWidget {
-  const SecondScreenWithData(String arguments, {super.key});
+  final String arguments;
+  const SecondScreenWithData(this.arguments, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen With Data'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text(arguments),
             ElevatedButton(
               child: const Text('Back'),
               onPressed: () {
