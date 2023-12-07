c79ff4ffc34427b1c87860613091bc74972224a1
tataadin <tataa2237@gmail.com>
2023-12-08 00:03:07 +0700

    [feature][navigation_learn] add new more screens

---
+++ lib/navigations_learn/return_data_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ReturnDataScreen extends StatefulWidget {
+  const ReturnDataScreen({super.key});
+
+  @override
+  State<ReturnDataScreen> createState() => _ReturnDataScreenState();
+}
+
+class _ReturnDataScreenState extends State<ReturnDataScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
