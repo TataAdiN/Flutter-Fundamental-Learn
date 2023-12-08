e782e9bf546d7ad3e895a24e53ec827690a5bb8f
tataadin <tataa2237@gmail.com>
2023-12-08 20:38:54 +0700

    [feature][modules] add new module news_app

---
+++ lib/news_app/news_app.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'models/article.dart';
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News App'),
+      ),
+      body: FutureBuilder<String>(
+        future:
+            DefaultAssetBundle.of(context).loadString('assets/articles.json'),
+        builder: (context, snapshot) {
+          final List<Article> articles = _parseArticles(snapshot.data);
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              return _buildArticleItem(context, articles[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  List<Article> _parseArticles(String? json) {
+    if (json == null) {
+      return [];
+    }
+    final List parsed = jsonDecode(json);
+    return parsed.map((json) => Article.fromJson(json)).toList();
+  }
+
+  Widget _buildArticleItem(BuildContext context, Article article) {
+    return ListTile(
+      contentPadding:
+          const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      leading: Image.network(
+        article.urlToImage,
+        width: 100,
+        errorBuilder: (ctx, error, _) => const Center(child: Icon(Icons.error)),
+      ),
+      title: Text(article.title),
+      subtitle: Text(article.author),
+    );
+  }
+}
