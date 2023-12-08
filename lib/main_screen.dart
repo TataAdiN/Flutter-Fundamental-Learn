8ffeba26ebaf69055fadc1d01d4b5c041c42d10f
tataadin <tataa2237@gmail.com>
2023-12-08 22:02:51 +0700

    [feature][modules] add new main_screen page

---
+++ lib/main_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text('Flutter Fundamental Learn'),
+      ),
+      body: ListView(
+        children: [
+          InkWell(
+            onTap: () => Navigator.pushNamed(context, '/fundamental_app'),
+            child: const Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: ListTile(
+                tileColor: Colors.white,
+                title: Text('Flutter Navigation'),
+                subtitle: Text('Flutter Navigation Fundamental Learning'),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () => Navigator.pushNamed(context, '/news_app'),
+            child: const Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: ListTile(
+                tileColor: Colors.white,
+                title: Text('News App'),
+                subtitle:
+                    Text('Flutter News App Example, Fundamental Learning'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
