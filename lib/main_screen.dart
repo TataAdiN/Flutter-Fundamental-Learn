4cb212f737823f12d5bbc9890457483f4d157475
tataadin <tataa2237@gmail.com>
2023-12-11 18:52:44 +0700

    remove unused item

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -8,44 +8,10 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text('Flutter Fundamental Learn'),
+        title: const Text('Indonesian Restaurant'),
       ),
       body: ListView(
         children: [
-          InkWell(
-            onTap: () => Navigator.pushNamed(context, '/fundamental_app'),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ListTile(
-                tileColor: Colors.white,
-                title: Text('Flutter Navigation'),
-                subtitle: Text('Flutter Navigation Fundamental Learning'),
-              ),
-            ),
-          ),
-          InkWell(
-            onTap: () => Navigator.pushNamed(context, '/news_app'),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ListTile(
-                tileColor: Colors.white,
-                title: Text('News App'),
-                subtitle:
-                    Text('Flutter News App Example, Fundamental Learning'),
-              ),
-            ),
-          ),
-          InkWell(
-            onTap: () => Navigator.pushNamed(context, '/flutter_ui'),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ListTile(
-                tileColor: Colors.white,
-                title: Text('Flutter UI'),
-                subtitle: Text('Flutter UI Design Fundamental Learning'),
-              ),
-            ),
-          )
         ],
       ),
     );
