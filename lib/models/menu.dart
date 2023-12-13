58e5125e8dcf4587f1c6bb95896a5d2d3f5f4ee8
tataadin <tataa2237@gmail.com>
2023-12-13 20:45:10 +0700

    [new][models] add restaurant, restaurant menu, and menu model

---
+++ lib/models/menu.dart
@@ -0,0 +1,12 @@
+class Menu {
+  String name;
+  Menu({required this.name});
+
+  factory Menu.fromJson(Map<String, dynamic> json) => Menu(
+        name: json['name'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+      };
+}
