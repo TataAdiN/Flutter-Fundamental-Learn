a183c66d4537b9ffe1c23a5fd7357927f3eb45b7
tataadin <tataa2237@gmail.com>
2023-12-16 06:02:19 +0700

    add new widget expandable app bar

---
+++ lib/screens/widgets/expandable_appbar_with_image.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/hideable_sliver_appbar_title.dart';
+
+class ExpandableAppbarWithImage extends StatelessWidget {
+  const ExpandableAppbarWithImage({
+    super.key,
+    required this.maxHeight,
+    required this.imgUrl,
+    required this.title,
+  });
+
+  final double maxHeight;
+  final String imgUrl, title;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      pinned: true,
+      leading: Container(
+        padding: const EdgeInsets.all(10),
+        child: ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            padding: EdgeInsets.zero,
+            backgroundColor: Colors.orange,
+            foregroundColor: Colors.deepOrangeAccent,
+          ),
+          child: const Icon(Icons.arrow_back, color: Colors.white),
+        ),
+      ),
+      expandedHeight: maxHeight,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Hero(
+          tag: imgUrl,
+          child: Image.network(
+            imgUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        collapseMode: CollapseMode.none,
+        centerTitle: true,
+        title: HideableSliverAppbarTitle(
+          child: Text(
+            title,
+          ),
+        ),
+      ),
+    );
+  }
+}
