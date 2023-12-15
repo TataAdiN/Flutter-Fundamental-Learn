58895ded398954001e676174079a2cde8f320f96
tataadin <tataa2237@gmail.com>
2023-12-16 06:03:44 +0700

    add new widget hideable appbar

---
+++ lib/screens/widgets/hideable_appbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/responsive.dart';
+
+class HideableAppBar extends StatelessWidget {
+  const HideableAppBar({
+    super.key,
+    required this.maxHeight,
+    required this.widget,
+  });
+  final double maxHeight;
+  final Widget widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      pinned: false,
+      snap: false,
+      floating: false,
+      expandedHeight: maxHeight,
+      elevation: 0,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(10),
+        title: widget,
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
