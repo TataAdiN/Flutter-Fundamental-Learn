e7de4e38b672ec78b81ea4d8a35bee352a9a7b69
tataadin <tataa2237@gmail.com>
2023-12-19 19:26:50 +0700

    add new module flutter_network

--- lib/flutter_network/flutter_network.dart
+++ lib/flutter_network/flutter_network.dart
@@ -1,73 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class FlutterUI extends StatelessWidget {
-  const FlutterUI({super.key});
+import 'album.dart';
+
+class FlutterNetwork extends StatefulWidget {
+  const FlutterNetwork({super.key});
+
+  @override
+  State<FlutterNetwork> createState() => _FlutterNetworkState();
+}
+
+class _FlutterNetworkState extends State<FlutterNetwork> {
+  late Future<Album> _futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureAlbum = getAlbum();
+  }
+
+  Future<Album> getAlbum() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+    if (response.statusCode == 200) {
+      return Album.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text('Flutter UI Fundamental'),
+        title: const Text('Flutter Network Fundamental'),
       ),
-      body: ListView(
-        children: [
-          InkWell(
-            onTap: () => Navigator.pushNamed(context, '/flutter_ui.layouts'),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ListTile(
-                tileColor: Colors.white,
-                title: Text('Layouts and Typography'),
-                subtitle: Text('Flutter Layouts and Typography Fundamental'),
-              ),
-            ),
-          ),
-          InkWell(
-            onTap: () => Navigator.pushNamed(context, '/flutter_ui.gestures'),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ListTile(
-                tileColor: Colors.white,
-                title: Text('Gestures'),
-                subtitle: Text('Flutter Gestures Fundamental'),
-              ),
-            ),
-          ),
-          InkWell(
-            onTap: () => Navigator.pushNamed(context, '/flutter_ui.calculator'),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ListTile(
-                tileColor: Colors.white,
-                title: Text('Calculator App'),
-                subtitle: Text('Flutter Calculator App for Layouting Example'),
-              ),
-            ),
-          ),
-          InkWell(
-            onTap: () => Navigator.pushNamed(context, '/flutter_ui.slivers'),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ListTile(
-                tileColor: Colors.white,
-                title: Text('Slivers'),
-                subtitle: Text('Flutter Slivers Fundamental'),
-              ),
-            ),
-          ),
-          InkWell(
-            onTap: () => Navigator.pushNamed(context, '/flutter_ui.slivers_list_grid'),
-            child: const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ListTile(
-                tileColor: Colors.white,
-                title: Text('Slivers List & Grid'),
-                subtitle: Text('Flutter Slivers List And Grid Fundamental'),
-              ),
-            ),
-          ),
-        ],
+      body: Center(
+        child: FutureBuilder<Album>(
+          future: _futureAlbum,
+          builder: (context, snapshot) {
+            var state = snapshot.connectionState;
+            if (state != ConnectionState.done) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Center(child: Text("${snapshot.error}"));
+              } else {
+                return const Text('');
+              }
+            }
+          },
+        ),
       ),
     );
   }
