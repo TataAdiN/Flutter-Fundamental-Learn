3f3e029e1070a592c46a029a1014024d87513327
tataadin <tataa2237@gmail.com>
2023-12-22 18:09:03 +0700

    implement provider

--- lib/news_app/news_app.dart
+++ lib/news_app/news_app.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'models/article_result.dart';
+import 'article_list_page.dart';
+import 'providers/new_provider.dart';
 import 'utils/api_service.dart';
-import 'widgets/card_article.dart';
 
 class NewsApp extends StatefulWidget {
   const NewsApp({super.key});
@@ -12,56 +13,13 @@ class NewsApp extends StatefulWidget {
 }
 
 class _NewsAppState extends State<NewsApp> {
-  late Future<ArticleResult> _article;
-
-  @override
-  void initState() {
-    super.initState();
-    _article = ApiService().topHeadlines();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('News App'),
+    return ChangeNotifierProvider<NewsProvider>(
+      create: (_) => NewsProvider(
+        apiService: ApiService(),
       ),
-      body: FutureBuilder<ArticleResult>(
-        future: _article,
-        builder: (context, AsyncSnapshot<ArticleResult> snapshot) {
-          var state = snapshot.connectionState;
-          if (state == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(
-                color: Colors.deepOrangeAccent,
-              ),
-            );
-          } else {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: snapshot.data?.articles.length,
-                itemBuilder: (context, index) {
-                  var article = snapshot.data?.articles[index];
-                  return CardArticle(article: article!);
-                },
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Material(
-                  child: Text(
-                    snapshot.error.toString(),
-                  ),
-                ),
-              );
-            } else {
-              return const Material(
-                child: Text(''),
-              );
-            }
-          }
-        },
-      ),
+      child: const ArticleListPage(),
     );
   }
 }
