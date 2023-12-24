19e4f6f3ddd4292ec0d64946cbb95fc0e114465d
tataadin <tataa2237@gmail.com>
2023-12-25 01:47:52 +0700

    temporary disable some code

--- lib/screens/widgets/restaurant_menu_section.dart
+++ lib/screens/widgets/restaurant_menu_section.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
-import '../../data/models/menu.dart';
-import 'menu_place_holder.dart';
-
 class RestaurantMenuSection extends StatelessWidget {
-  const RestaurantMenuSection({super.key, required this.menus, required this.icon});
-  final List<Menu> menus;
+  const RestaurantMenuSection({super.key, required this.icon});
+  //final List<Menu> menus;
   final IconData icon;
 
   @override
@@ -15,7 +12,7 @@ class RestaurantMenuSection extends StatelessWidget {
       sliver: SliverGrid.count(
         crossAxisSpacing: 5.0,
         mainAxisSpacing: 5.0,
-        crossAxisCount: 3,
+        crossAxisCount: 3,/*
         children: menus
             .map(
               (food) => MenuPlaceHolder(
@@ -23,7 +20,7 @@ class RestaurantMenuSection extends StatelessWidget {
                 menu: food.name,
               ),
             )
-            .toList(),
+            .toList(),*/
       ),
     );
   }
