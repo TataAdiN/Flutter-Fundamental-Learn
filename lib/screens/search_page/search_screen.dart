18e4b95703ec22d326f6cfb922b49e34821850a5
tataadin <tataa2237@gmail.com>
2023-12-25 03:41:32 +0700

    [screen.search] fix implementation search provider and implement lottie loading animation

--- lib/screens/search_page/search_screen.dart
+++ lib/screens/search_page/search_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
-import '../../providers/restaurant_provider.dart';
+import '../../data/enums/search_result_state.dart';
+import '../../data/models/restaurant.dart';
+import '../../providers/search_restaurant_provider.dart';
 import '../../utils/responsive.dart';
 import '../../widgets/pinned_searchbar.dart';
 import '../main_page/widgets/custom_sliver_appbar.dart';
+import '../main_page/widgets/restaurant_card.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -28,12 +32,46 @@ class SearchScreen extends StatelessWidget {
             slivers: [
               _appBar(context, screenHeight),
               PinnedSearchBar(
-                onChanged: (String queryParam) => Provider.of<RestaurantProvider>(context, listen: false).searchRestaurant(queryParam),
+                onChanged: (String queryParam) =>
+                    Provider.of<SearchRestaurantProvider>(context,
+                            listen: false)
+                        .searchRestaurant(queryParam),
                 width: Responsive.adjust(
                   screenSize: screenWidth,
                   percentage: 100,
                 ),
-              )
+              ),
+              Consumer<SearchRestaurantProvider>(builder: (
+                BuildContext context,
+                SearchRestaurantProvider provider,
+                _,
+              ) {
+                if (provider.state == SearchResultState.loading) {
+                  return SliverToBoxAdapter(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        top: Responsive.adjust(
+                            screenSize: screenHeight, percentage: 10),
+                      ),
+                      child: Column(
+                        children: [
+                          LottieBuilder.asset(
+                            'assets/food_search.json',
+                            width: 120,
+                            height: 120,
+                            animate: true,
+                          ),
+                          const Text("Looking for a suitable restaurant")
+                        ],
+                      ),
+                    ),
+                  );
+                } else if (provider.state == SearchResultState.hasData) {
+                  return _content(provider.result.restaurants, screenWidth);
+                }
+                return const SliverToBoxAdapter(
+                );
+              }),
             ],
           ),
         ),
@@ -54,5 +92,37 @@ class SearchScreen extends StatelessWidget {
           textScaler: TextScaler.linear(1),
         ),
         isCenter: true,
+      );
+
+  Widget _content(List<Restaurant> restaurants, double screenWidth) =>
+      SliverPadding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 5,
+        ),
+        sliver: SliverList.builder(
+          itemCount: restaurants.length,
+          itemBuilder: (
+            BuildContext context,
+            int index,
+          ) {
+            return RestaurantCard(
+              onTap: () => Navigator.pushNamed(
+                context,
+                '/restaurant',
+                arguments: restaurants[index],
+              ),
+              restaurant: restaurants[index],
+              imgWidth: Responsive.adjust(
+                screenSize: screenWidth,
+                percentage: 20,
+              ),
+              imgHeight: Responsive.adjust(
+                screenSize: screenWidth,
+                percentage: 15,
+              ),
+            );
+          },
+        ),
       );
 }
