27390586c980f0ea23bf1eab9bff1feb30768493
tataadin <tataa2237@gmail.com>
2023-12-25 02:16:33 +0700

    [widgets] add new widget pinned_searchbar

---
+++ lib/widgets/pinned_searchbar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+
+import 'sliver_pinned_header.dart';
+
+class PinnedSearchBar extends StatelessWidget {
+  PinnedSearchBar({super.key, required this.onChanged, required this.width});
+
+  final double width;
+  Function(String) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPinnedHeader(
+      minHeight: 72,
+      maxHeight: 72,
+      widget: Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  width: width,
+                  child: Align(
+                    child: CupertinoSearchTextField(
+                      placeholder: 'Type restaurant name here...',
+                      onChanged: onChanged,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
