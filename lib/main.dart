2b8d25bcca53c06af4d67efcaf3d7c6cce75fafe
tataadin <tataa2237@gmail.com>
2023-12-25 02:18:04 +0700

    [main] register new route [da78b36b61f8ecbb204635602cafe9acc86819f8] and add textselectiontheme to make sure cupertino cursor colored with accent color

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'data/models/restaurant.dart';
-import 'screens/main_screen.dart';
+import 'screens/main_page/main_page.dart';
 import 'screens/restaurant_screen.dart';
+import 'screens/search_page/search_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,18 +18,24 @@ class MyApp extends StatelessWidget {
       title: 'Indonesian Restaurant',
       theme: ThemeData(
         useMaterial3: true,
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.deepOrangeAccent,
+          selectionColor: Colors.deepOrangeAccent,
+          selectionHandleColor: Colors.deepOrangeAccent,
+        ),
         colorScheme: Theme.of(context).colorScheme.copyWith(
               primary: Colors.white,
               onPrimary: Colors.black,
-              secondary: Colors.orange,
+              secondary: Colors.deepOrangeAccent,
             ),
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const MainScreen(),
+        '/': (context) => const MainPage(),
         '/restaurant': (context) => RestaurantScreen(
             restaurant:
                 ModalRoute.of(context)?.settings.arguments as Restaurant),
+        '/search': (context) => const SearchPage(),
       },
     );
   }
