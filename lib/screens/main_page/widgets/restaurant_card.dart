6acb184d0f80ed9eb40f1b512d24a4f19246c3b1
tataadin <tataa2237@gmail.com>
2023-12-25 01:47:12 +0700

    [main_page.widget.restaurant_card] improve layout and add restaurant description

--- lib/screens/main_page/widgets/restaurant_card.dart
+++ lib/screens/main_page/widgets/restaurant_card.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fundamental/data/api_service.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:line_icons/line_icons.dart';
 
-import '../../data/models/restaurant.dart';
+import '../../../data/api_service.dart';
+import '../../../data/models/restaurant.dart';
 
 class RestaurantCard extends StatelessWidget {
   const RestaurantCard({
@@ -27,47 +27,27 @@ class RestaurantCard extends StatelessWidget {
           padding: const EdgeInsets.all(10.0),
           child: Row(
             children: [
-              Hero(
-                tag: ApiService.imageUrl(restaurant.pictureId),
-                child: Container(
-                  height: imgHeight,
-                  width: imgWidth,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2, color: Colors.grey),
-                    shape: BoxShape.rectangle,
-                    borderRadius: const BorderRadius.all(Radius.circular(10)),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(ApiService.imageUrl(restaurant.pictureId)),
+              Column(
+                children: [
+                  Hero(
+                    tag: ApiService.imageUrl(restaurant.pictureId),
+                    child: Container(
+                      height: imgHeight,
+                      width: imgWidth,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 2, color: Colors.grey),
+                        shape: BoxShape.rectangle,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              ApiService.imageUrl(restaurant.pictureId)),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                width: 16,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    restaurant.name,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  Row(
-                    children: [
-                      const Icon(LineIcons.mapMarked, size: 20),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        restaurant.city,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
+                  SizedBox(height: 4),
                   RatingBarIndicator(
                     rating: restaurant.rating.roundToDouble(),
                     itemCount: 5,
@@ -76,9 +56,44 @@ class RestaurantCard extends StatelessWidget {
                       Icons.star,
                       color: Colors.amber,
                     ),
-                    itemSize: 20,
+                    itemSize: 12,
                   ),
                 ],
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      restaurant.name,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    Row(
+                      children: [
+                        const Icon(LineIcons.mapMarked, size: 20),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          restaurant.city,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      restaurant.description,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                      softWrap: true,
+                    ),
+                  ],
+                ),
               )
             ],
           ),
