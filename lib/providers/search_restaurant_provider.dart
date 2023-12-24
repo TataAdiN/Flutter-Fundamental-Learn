6000cc2473f233edea05b5ea7b5b47142ac1839b
tataadin <tataa2237@gmail.com>
2023-12-25 03:36:55 +0700

    [provider] rename to search_restaurant

--- lib/providers/search_restaurant_provider.dart
+++ lib/providers/search_restaurant_provider.dart
@@ -1,44 +1,44 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamental/data/enums/search_result_state.dart';
 
 import '../data/api_service.dart';
-import '../data/enums/result_state.dart';
 import '../data/models/search_result.dart';
 
-class RestaurantProvider extends ChangeNotifier {
+class SearchRestaurantProvider extends ChangeNotifier {
   final ApiService apiService;
 
-  RestaurantProvider({required this.apiService});
+  SearchRestaurantProvider({required this.apiService});
 
   late SearchResult _searchResult;
-  late ResultState _state;
+  SearchResultState _state = SearchResultState.idle;
   String _message = '';
 
   SearchResult get result => _searchResult;
   String get message => _message;
-  ResultState get state => _state;
+  SearchResultState get state => _state;
 
   void searchRestaurant(String searchQuery) => _search(searchQuery);
 
   Future<dynamic> _search(String searchQuery) async {
     try {
-      _state = ResultState.loading;
+      _state = SearchResultState.loading;
       notifyListeners();
       final result = await apiService.findByName(searchQuery);
       if (result.restaurants.isEmpty) {
-        _state = ResultState.noData;
+        _state = SearchResultState.noData;
         notifyListeners();
         return _message = 'Empty Data';
       } else {
-        _state = ResultState.hasData;
+        _state = SearchResultState.hasData;
         notifyListeners();
         return _searchResult = result;
       }
     } on SocketException catch (_) {
-      _state = ResultState.noInternet;
+      _state = SearchResultState.noInternet;
     } catch (e) {
-      _state = ResultState.error;
+      _state = SearchResultState.error;
       notifyListeners();
       return _message = 'Error --> $e';
     }
