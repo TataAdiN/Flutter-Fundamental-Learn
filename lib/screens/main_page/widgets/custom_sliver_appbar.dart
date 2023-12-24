d5656c6bfe17c6371a128d6c24910a3f3da56c3f
tataadin <tataa2237@gmail.com>
2023-12-25 01:38:56 +0700

    [main_page.widget.custom_sliverappbar] rename from hideableappbar and add more customable behavior

--- lib/screens/main_page/widgets/custom_sliver_appbar.dart
+++ lib/screens/main_page/widgets/custom_sliver_appbar.dart
@@ -1,35 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:line_icons/line_icons.dart';
 
-class HideableAppBar extends StatelessWidget {
-  const HideableAppBar({
+class CustomSliverAppBar extends StatelessWidget {
+  const CustomSliverAppBar({
     super.key,
     required this.maxHeight,
     required this.widget,
+    this.isPinned = false,
+    this.isCenter = false,
+    this.showBackNav = false,
+    this.actions,
   });
+  final bool isPinned;
+  final bool isCenter;
+  final bool showBackNav;
   final double maxHeight;
   final Widget widget;
+  final List<Widget>? actions;
 
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      pinned: true,
+      pinned: isPinned,
       snap: false,
       floating: false,
       expandedHeight: maxHeight,
       elevation: 0,
       flexibleSpace: FlexibleSpaceBar(
-        centerTitle: false,
-        titlePadding: const EdgeInsets.all(10),
+        centerTitle: isCenter,
+        titlePadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         title: widget,
       ),
-      automaticallyImplyLeading: false,
+      automaticallyImplyLeading: showBackNav,
       backgroundColor: Colors.white,
-      actions: const [
-        Padding(
-          padding: EdgeInsets.only(right: 20),
-            child: Icon(LineIcons.search, size: 32,))
-      ],
+      actions: actions,
     );
   }
 }
