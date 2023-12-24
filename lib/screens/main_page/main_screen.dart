586bc66aa38296674a3e9e307261013ad2bf1fa5
tataadin <tataa2237@gmail.com>
2023-12-25 01:34:18 +0700

    [screen.main_screen] remove unused code and improve code implementation

--- lib/screens/main_page/main_screen.dart
+++ lib/screens/main_page/main_screen.dart
@@ -1,31 +1,20 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:provider/provider.dart';
 
-import '../data/enums/result_state.dart';
-import '../data/models/restaurant.dart';
-import '../providers/all_restaurant_provider.dart';
-import '../utils/responsive.dart';
-import '../widgets/sliver_pinned_header.dart';
-import 'widgets/hideable_appbar.dart';
+import '../../data/enums/result_state.dart';
+import '../../data/models/restaurant.dart';
+import '../../providers/all_restaurant_provider.dart';
+import '../../utils/responsive.dart';
+import 'widgets/custom_sliver_appbar.dart';
 import 'widgets/restaurant_card.dart';
 
-class AllRestaurantScreen extends StatefulWidget {
-  const AllRestaurantScreen({super.key});
+class MainScreen extends StatelessWidget {
+  MainScreen({super.key});
 
-  @override
-  State<AllRestaurantScreen> createState() => _AllRestaurantScreenState();
-}
-
-class _AllRestaurantScreenState extends State<AllRestaurantScreen> {
   List<Restaurant> restaurants = [];
   List<Restaurant> persistRestaurants = [];
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,29 +33,34 @@ class _AllRestaurantScreenState extends State<AllRestaurantScreen> {
           child: CustomScrollView(
             slivers: [
               _appBar(context, screenHeight),
-            Consumer<AllRestaurantProvider>(
-                builder: (context, state, _) {
-                  if (state.state == ResultState.loading) {
-                    return SliverToBoxAdapter(
-                      child: Padding(
-                        padding: EdgeInsets.only(top: Responsive.adjust(screenSize: screenHeight, percentage: 10)),
-                        child: const Center(
-                          child: CircularProgressIndicator(
-                            color: Colors.deepOrangeAccent,
-                          ),
+              Consumer<AllRestaurantProvider>(builder: (context, state, _) {
+                if (state.state == ResultState.loading) {
+                  return SliverToBoxAdapter(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        top: Responsive.adjust(
+                            screenSize: screenHeight, percentage: 10),
+                      ),
+                      child: const Center(
+                        child: CircularProgressIndicator(
+                          color: Colors.deepOrangeAccent,
                         ),
                       ),
-                    );
-                  }else if (state.state == ResultState.hasData) {
-                    return _content(state.result.restaurants, screenWidth);
-                  }
-                  else if (state.state == ResultState.noData) {
-                    return _emptyContent();
-                  }
-                  return Container(
+                    ),
+                  );
+                } else if (state.state == ResultState.hasData) {
+                  return _content(state.result.restaurants, screenWidth);
+                } else if (state.state == ResultState.noData) {
+                  return _emptyContent();
+                } else if (state.state == ResultState.error) {
+                  return SliverToBoxAdapter(
+                    child: Center(
+                      child: Text(state.message),
+                    ),
                   );
                 }
-            )
+                return const SliverToBoxAdapter();
+              })
             ],
           ),
         ),
@@ -81,9 +75,6 @@ class _AllRestaurantScreenState extends State<AllRestaurantScreen> {
         filtered.add(item);
       }
     }
-    setState(() {
-      restaurants = filtered;
-    });
   }
 
   Widget _content(List<Restaurant> restaurants, double screenWidth) =>
@@ -98,65 +89,29 @@ class _AllRestaurantScreenState extends State<AllRestaurantScreen> {
             BuildContext context,
             int index,
           ) {
-            return _itemCard(restaurants[index], screenWidth);
+            return RestaurantCard(
+              onTap: () => Navigator.pushNamed(
+                context,
+                '/restaurant',
+                arguments: restaurants[index],
+              ),
+              restaurant: restaurants[index],
+              imgWidth: Responsive.adjust(
+                screenSize: screenWidth,
+                percentage: 20,
+              ),
+              imgHeight: Responsive.adjust(
+                screenSize: screenWidth,
+                percentage: 15,
+              ),
+            );
           },
         ),
       );
 
-  Widget _itemCard(Restaurant restaurant, double screenWidth) => RestaurantCard(
-        onTap: () => Navigator.pushNamed(
-          context,
-          '/restaurant',
-          arguments: restaurant,
-        ),
-        restaurant: restaurant,
-        imgWidth: Responsive.adjust(
-          screenSize: screenWidth,
-          percentage: 20,
-        ),
-        imgHeight: Responsive.adjust(
-          screenSize: screenWidth,
-          percentage: 15,
-        ),
-      );
-
-  Widget _pinnedHeading(double screenHeight, double screenWidth) =>
-      SliverPinnedHeader(
-        minHeight: Responsive.adjust(
-          screenSize: screenHeight,
-          percentage: 9,
-        ),
-        maxHeight: Responsive.adjust(
-          screenSize: screenHeight,
-          percentage: 9,
-        ),
-        widget: Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(left: 10, right: 10),
-                    width: Responsive.adjust(
-                      screenSize: screenWidth,
-                      percentage: 100,
-                    ),
-                    child: Align(
-                      child: CupertinoSearchTextField(
-                        placeholder: 'Type restaurant name here...',
-                        onChanged: (String queryParam) => _filter(queryParam),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      );
-
-  Widget _appBar(BuildContext context, double screenHeight) => HideableAppBar(
+  Widget _appBar(BuildContext context, double screenHeight) =>
+      CustomSliverAppBar(
+        isPinned: true,
         maxHeight: Responsive.adjust(
           screenSize: screenHeight,
           percentage: 12,
@@ -176,6 +131,18 @@ class _AllRestaurantScreenState extends State<AllRestaurantScreen> {
             ),
           ],
         ),
+        actions: [
+          InkWell(
+            onTap: () => Navigator.pushNamed(context, '/search'),
+            child: const Padding(
+              padding: EdgeInsets.only(right: 40, top: 5),
+              child: Icon(
+                LineIcons.search,
+                size: 32,
+              ),
+            ),
+          )
+        ],
       );
 
   Widget _emptyContent() => const SliverToBoxAdapter(
