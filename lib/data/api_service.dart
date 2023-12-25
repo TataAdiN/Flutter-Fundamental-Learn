28ee655edadfc288e424d8066640b208ebca18ec
tataadin <tataa2237@gmail.com>
2023-12-26 01:19:14 +0700

    [data.api_service] add new function get

--- lib/data/api_service.dart
+++ lib/data/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 import 'models/all_restaurant_result.dart';
+import 'models/restaurant_result.dart';
 import 'models/search_result.dart';
 
 class ApiService {
@@ -26,6 +27,15 @@ class ApiService {
       return SearchResult.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to search restaurant');
+    }
+  }
+
+  Future<RestaurantResult> get(String restaurantId) async {
+    final response = await http.get(Uri.parse("$_baseUrl/detail/$restaurantId"));
+    if (response.statusCode == 200) {
+      return RestaurantResult.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to get restaurant with id: $restaurantId');
     }
   }
 }
