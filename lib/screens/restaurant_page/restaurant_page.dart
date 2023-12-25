d0ba3542e3f796bc2bb11e8db0d0592abfa3927d
tataadin <tataa2237@gmail.com>
2023-12-25 15:34:51 +0700

    [screens] add new page restuarant_page

---
+++ lib/screens/restaurant_page/restaurant_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RestaurantPage extends StatelessWidget {
+  const RestaurantPage({super.key, required this.restaurantId});
+
+  final String restaurantId;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
