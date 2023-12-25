98f1e60fa89cbcac21ce7b5d81e472a6808e75e3
tataadin <tataa2237@gmail.com>
2023-12-25 15:32:48 +0700

    [screen.widgets] add new widget restaurant_sliver_list

---
+++ lib/screens/widgets/restaurant_sliver_list.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../data/models/restaurant.dart';
+import '../../utils/responsive.dart';
+import '../main_page/widgets/restaurant_card.dart';
+
+class RestaurantSliverList extends StatelessWidget {
+  const RestaurantSliverList({
+    super.key,
+    required this.restaurants,
+    required this.screenWidth,
+  });
+
+  final List<Restaurant> restaurants;
+  final double screenWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+        horizontal: 8,
+      ),
+      sliver: SliverList.builder(
+        itemCount: restaurants.length,
+        itemBuilder: (
+          BuildContext context,
+          int index,
+        ) {
+          return RestaurantCard(
+            onTap: () => Navigator.pushNamed(
+              context,
+              '/restaurant',
+              arguments: restaurants[index].id,
+            ),
+            restaurant: restaurants[index],
+            imgWidth: Responsive.adjust(
+              screenSize: screenWidth,
+              percentage: 20,
+            ),
+            imgHeight: Responsive.adjust(
+              screenSize: screenWidth,
+              percentage: 15,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
