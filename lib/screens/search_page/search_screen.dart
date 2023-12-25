aaa886b8d77f8703ef42e7a57914d86fd8eb4338
tataadin <tataa2237@gmail.com>
2023-12-25 15:40:41 +0700

    [screen.search] fix implementation with sliver_list[98f1e60fa89cbcac21ce7b5d81e472a6808e75e3] and search_animation [f1ebf7b575e6686e580e819940e0d3bd3f1a6635]

--- lib/screens/search_page/search_screen.dart
+++ lib/screens/search_page/search_screen.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
+import '../../component_widgets/pinned_searchbar.dart';
 import '../../data/enums/search_result_state.dart';
-import '../../data/models/restaurant.dart';
 import '../../providers/search_restaurant_provider.dart';
 import '../../utils/responsive.dart';
-import '../../widgets/pinned_searchbar.dart';
 import '../main_page/widgets/custom_sliver_appbar.dart';
-import '../main_page/widgets/restaurant_card.dart';
+import '../widgets/restaurant_sliver_list.dart';
+import 'widgets/search_animation.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -33,9 +32,10 @@ class SearchScreen extends StatelessWidget {
               _appBar(context, screenHeight),
               PinnedSearchBar(
                 onChanged: (String queryParam) =>
-                    Provider.of<SearchRestaurantProvider>(context,
-                            listen: false)
-                        .searchRestaurant(queryParam),
+                    Provider.of<SearchRestaurantProvider>(
+                  context,
+                  listen: false,
+                ).searchRestaurant(queryParam),
                 width: Responsive.adjust(
                   screenSize: screenWidth,
                   percentage: 100,
@@ -48,29 +48,18 @@ class SearchScreen extends StatelessWidget {
               ) {
                 if (provider.state == SearchResultState.loading) {
                   return SliverToBoxAdapter(
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                        top: Responsive.adjust(
-                            screenSize: screenHeight, percentage: 10),
-                      ),
-                      child: Column(
-                        children: [
-                          LottieBuilder.asset(
-                            'assets/food_search.json',
-                            width: 120,
-                            height: 120,
-                            animate: true,
-                          ),
-                          const Text("Looking for a suitable restaurant")
-                        ],
-                      ),
+                    child: SearchAnimation(
+                      paddingTop: Responsive.adjust(
+                          screenSize: screenHeight, percentage: 10),
                     ),
                   );
                 } else if (provider.state == SearchResultState.hasData) {
-                  return _content(provider.result.restaurants, screenWidth);
+                  return RestaurantSliverList(
+                    restaurants: provider.result.restaurants,
+                    screenWidth: screenWidth,
+                  );
                 }
-                return const SliverToBoxAdapter(
-                );
+                return const SliverToBoxAdapter();
               }),
             ],
           ),
@@ -92,37 +81,5 @@ class SearchScreen extends StatelessWidget {
           textScaler: TextScaler.linear(1),
         ),
         isCenter: true,
-      );
-
-  Widget _content(List<Restaurant> restaurants, double screenWidth) =>
-      SliverPadding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 20,
-          horizontal: 5,
-        ),
-        sliver: SliverList.builder(
-          itemCount: restaurants.length,
-          itemBuilder: (
-            BuildContext context,
-            int index,
-          ) {
-            return RestaurantCard(
-              onTap: () => Navigator.pushNamed(
-                context,
-                '/restaurant',
-                arguments: restaurants[index],
-              ),
-              restaurant: restaurants[index],
-              imgWidth: Responsive.adjust(
-                screenSize: screenWidth,
-                percentage: 20,
-              ),
-              imgHeight: Responsive.adjust(
-                screenSize: screenWidth,
-                percentage: 15,
-              ),
-            );
-          },
-        ),
       );
 }
