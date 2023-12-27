c08ecc90d8ded8c573dcd1ec7b7c8d6a959e877b
tataadin <tataa2237@gmail.com>
2023-12-27 21:24:08 +0700

    [screen.restaurant] implement with new widget [e53484b85adf3347518c5fc916b30a460f6d4d58][0675e0851570b7e48d1467ccf3059d7850d9d7be][4c73c43141f2143a5bee163c3502689ea54aeae1]

--- lib/screens/restaurant_page/restaurant_screen.dart
+++ lib/screens/restaurant_page/restaurant_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:line_icons/line_icons.dart';
 import 'package:provider/provider.dart';
-import 'package:readmore/readmore.dart';
 
 import '../../component_widgets/expandable_appbar_with_image.dart';
 import '../../component_widgets/sliver_pinned_header.dart';
@@ -12,7 +9,10 @@ import '../../data/models/restaurant.dart';
 import '../../providers/restaurant_provider.dart';
 import '../../utils/responsive.dart';
 import '../widgets/food_loading.dart';
-import '../widgets/restaurant_menu_section.dart';
+import 'widgets/pinned_menu_heading.dart';
+import 'widgets/restaurant_content.dart';
+import 'widgets/restaurant_menu_section.dart';
+import 'widgets/restaurant_not_found.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
@@ -36,45 +36,10 @@ class RestaurantScreen extends StatelessWidget {
                 ),
               );
             } else if (provider.state == RestaurantResultState.notExist) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.sentiment_dissatisfied,
-                      size: 64,
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    const Text(
-                      'Restuarant does not exist',
-                      style: TextStyle(color: Colors.black54),
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor:
-                            Theme.of(context).colorScheme.secondary,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(12),
-                          ),
-                        ),
-                      ),
-                      child: const Text('Back'),
-                      onPressed: () => Navigator.of(context).pop(),
-                    )
-                  ],
-                ),
-              );
+              return const RestaurantNotFound();
             } else if (provider.state == RestaurantResultState.exist) {
               Restaurant? restaurant = provider.result.restaurant;
-              return buildNestedScrollView(restaurant!, screenHeight);
+              return _buildRestaurant(restaurant!, screenHeight);
             }
             return Container();
           },
@@ -83,8 +48,10 @@ class RestaurantScreen extends StatelessWidget {
     );
   }
 
-  NestedScrollView buildNestedScrollView(
-      Restaurant restaurant, double screenHeight) {
+  NestedScrollView _buildRestaurant(
+    Restaurant restaurant,
+    double screenHeight,
+  ) {
     return NestedScrollView(
       headerSliverBuilder: (context, isScrolled) {
         return [
@@ -125,63 +92,13 @@ class RestaurantScreen extends StatelessWidget {
               percentage: 8,
             ),
           ),
-          _content(restaurant),
-          SliverPinnedHeader(
-            widget: Container(
-              color: Colors.white,
-              child: const Align(
-                child: Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Row(
-                    children: [
-                      SizedBox(width: 10),
-                      Icon(Icons.restaurant_sharp, size: 24),
-                      SizedBox(width: 10),
-                      Text(
-                        "Foods",
-                        style: TextStyle(
-                          color: Colors.black54,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            minHeight: 32,
-            maxHeight: 32,
-          ),
+          RestaurantContent(restaurant: restaurant),
+          const PinnedMenuHeading(headingTitle: "Foods"),
           RestaurantMenuSection(
             menus: restaurant.menus!.foods,
             icon: Icons.restaurant_sharp,
           ),
-          SliverPinnedHeader(
-            widget: Container(
-              color: Colors.white,
-              child: const Align(
-                child: Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Row(
-                    children: [
-                      SizedBox(width: 10),
-                      Icon(Icons.coffee_outlined, size: 24),
-                      SizedBox(width: 10),
-                      Text(
-                        "Drinks",
-                        style: TextStyle(
-                          color: Colors.black54,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            minHeight: 32,
-            maxHeight: 32,
-          ),
+          const PinnedMenuHeading(headingTitle: "Drinks"),
           RestaurantMenuSection(
             menus: restaurant.menus!.drinks,
             icon: Icons.coffee_outlined,
@@ -190,59 +107,4 @@ class RestaurantScreen extends StatelessWidget {
       ),
     );
   }
-
-  Widget _content(Restaurant restaurant) => SliverList(
-        delegate: SliverChildListDelegate(
-          [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(LineIcons.mapMarked, size: 28),
-                const SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  restaurant.city,
-                  style: const TextStyle(fontSize: 18),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Center(
-              child: RatingBarIndicator(
-                rating: restaurant.rating.roundToDouble(),
-                itemCount: 5,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
-                itemBuilder: (context, _) => const Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                ),
-                itemSize: 20,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: ReadMoreText(
-                restaurant.description,
-                textAlign: TextAlign.justify,
-                trimLines: 3,
-                trimMode: TrimMode.Line,
-                trimCollapsedText: 'Show more',
-                trimExpandedText: 'Show less',
-                moreStyle: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orange,
-                ),
-                lessStyle: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orange,
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
 }
