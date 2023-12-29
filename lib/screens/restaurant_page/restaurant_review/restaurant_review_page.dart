ca46781689d79a025678d7b4a4586e9048872e50
tataadin <tataa2237@gmail.com>
2023-12-30 03:52:49 +0700

    [screen.restaurant_page] fix implement new paramater

--- lib/screens/restaurant_page/restaurant_review/restaurant_review_page.dart
+++ lib/screens/restaurant_page/restaurant_review/restaurant_review_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../data/api_service.dart';
 import '../../../data/models/restaurant.dart';
 import '../../../providers/restaurant_review_provider.dart';
 import 'restaurant_review_screen.dart';
@@ -16,7 +17,11 @@ class RestaurantReviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<RestaurantReviewProvider>(
-      create: (_) => RestaurantReviewProvider(reviews: restaurant.customerReviews!),
+      create: (_) => RestaurantReviewProvider(
+        reviews: restaurant.customerReviews!,
+        restaurantId: restaurant.id,
+        apiService: ApiService(),
+      ),
       child: RestaurantReviewScreen(
         restaurantName: restaurant.name,
       ),
