d6db29567002648a31d22c17625e45187a900677
tataadin <tataa2237@gmail.com>
2023-12-30 03:54:01 +0700

    [restaurant.widgets] add new widget restaurant_review_list

---
+++ lib/screens/restaurant_page/restaurant_review/widgets/restaurant_review_list.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../../../../data/models/customer_review.dart';
+
+class RestaurantReviewList extends StatelessWidget {
+  const RestaurantReviewList({
+    super.key,
+    required this.reviews,
+  });
+
+  final List<CustomerReview> reviews;
+
+  @override
+  Widget build(BuildContext context) => SliverPadding(
+    padding: const EdgeInsets.symmetric(
+      vertical: 20,
+      horizontal: 8,
+    ),
+    sliver: SliverList.builder(
+      itemCount: reviews.length,
+      itemBuilder: (
+          BuildContext context,
+          int index,
+          ) {
+        return Card(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  reviews[index].name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(reviews[index].review),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(reviews[index].date),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
