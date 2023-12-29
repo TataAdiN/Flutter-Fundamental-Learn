99e6d14fce0edf9cea13cd56f79dc7ac28d4affb
tataadin <tataa2237@gmail.com>
2023-12-30 03:54:14 +0700

    [restaurant.widgets] add new widget review_comment_box

---
+++ lib/screens/restaurant_page/restaurant_review/widgets/review_comment_box.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class ReviewCommentBox extends StatelessWidget {
+
+  ReviewCommentBox({
+    super.key, required this.onTypeName, required this.onTypeReview, required this.onTapSend,
+  });
+
+  final TextEditingController reviewNameControl = TextEditingController();
+  final TextEditingController reviewTextControl = TextEditingController();
+
+  final Function(String name) onTypeName;
+  final Function(String review) onTypeReview;
+  final Function(TextEditingController fieldName, TextEditingController fieldReview) onTapSend;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        width: double.infinity,
+        height: 120,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            top: BorderSide(
+              color: Colors.deepOrangeAccent,
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextField(
+                controller: reviewNameControl,
+                onChanged: (String text) => onTypeName(text),
+                style: const TextStyle(fontSize: 16),
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.only(left: 16),
+                  hintText: 'Your Name..',
+                  hintStyle: TextStyle(fontSize: 16),
+                  border: InputBorder.none,
+                  isDense: true,
+                ),
+              ),
+              const Divider(),
+              TextField(
+                controller: reviewTextControl,
+                minLines: 1,
+                maxLines: 10,
+                onChanged: (String text) => onTypeReview(text),
+                style: const TextStyle(fontSize: 14),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffix: TextButton(
+                    onPressed: () => onTapSend(reviewNameControl, reviewTextControl),
+                    child: const Text(
+                      'Send Review',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                    ),
+                  ),
+                  hintText: 'Write your review...',
+                  hintStyle: const TextStyle(fontSize: 14),
+                  isDense: true,
+                  contentPadding: const EdgeInsets.only(left: 16),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
