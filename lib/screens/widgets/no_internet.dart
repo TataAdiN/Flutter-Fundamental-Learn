452c65a8ffa53ccd13062c779f4786cde469dda9
tataadin <tataa2237@gmail.com>
2023-12-30 13:22:56 +0700

    [widgets] add new widget no_internet

---
+++ lib/screens/widgets/no_internet.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../component_widgets/rounded_button.dart';
+
+class NoInternet extends StatelessWidget {
+  const NoInternet({super.key, required this.onRetry});
+
+  final VoidCallback onRetry;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.sentiment_dissatisfied,
+              size: 64,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Text(
+              'No internet detected',
+              style: TextStyle(
+                color: Colors.black54,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'Please ensure your network has internet access',
+              style: TextStyle(color: Colors.black54),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            RoundedButton(
+              onTap: () => onRetry(),
+              content: const Text('Try Again'),
+            ),
+          ],
+        ),
+    );
+  }
+}
