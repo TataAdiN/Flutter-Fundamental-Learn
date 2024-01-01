58324d1731686d38a33ab3d4f53b958dfdafd113
tataadin <tataa2237@gmail.com>
2024-01-01 19:05:42 +0700

    remove unused code and register counter[29c315453b1b943914213bed99e5e4a27f311238] route

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,11 @@
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_fundamental/screens/news_app/utils/notification_helper.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-import 'screens/alarm/alarm_screen.dart';
-import 'screens/alarm/services/background_service.dart';
+import 'screens/counter/counter_screen.dart';
 import 'screens/main_screen.dart';
-import 'screens/news_app/news_app.dart';
 import 'screens/notification/detail_notification_screen.dart';
 import 'screens/notification/notification_screen.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-FlutterLocalNotificationsPlugin();
-
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final BackgroundService service = BackgroundService();
-  service.initializeIsolate();
-
-  AndroidAlarmManager.initialize();
-  final NotificationHelper notificationHelper = NotificationHelper();
-  await notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
   runApp(const MyApp());
 }
 
@@ -30,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'State Management',
+      title: 'Local Storage',
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: Theme.of(context).colorScheme.copyWith(
@@ -44,8 +29,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => const MainScreen(),
         '/notification': (context) => const NotificationScreen(),
         '/notification.detail': (context) => const DetailNotificationScreen(),
-        '/alarm': (context) => const AlarmScreen(),
-        '/news_app': (context) => const NewsApp(),
+        '/counter': (context) => const CounterScreen(),
       },
     );
   }
