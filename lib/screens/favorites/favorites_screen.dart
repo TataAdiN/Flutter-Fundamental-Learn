5fa54522b1f0ccecaf0e3af288d79616b722d427
tataadin <tataa2237@gmail.com>
2024-01-09 20:40:51 +0700

    rename from bookmarks to favorites

--- lib/screens/favorites/favorites_screen.dart
+++ lib/screens/favorites/favorites_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class BookmarksScreen extends StatelessWidget {
-  const BookmarksScreen({super.key});
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Bookmarks'),
+          title: const Text('Favorites'),
         ),
         body: Container());
   }
