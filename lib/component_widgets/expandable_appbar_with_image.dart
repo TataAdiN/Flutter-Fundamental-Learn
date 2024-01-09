918b9c949cc427099354d50476e347ddf23e163d
tataadin <tataa2237@gmail.com>
2024-01-09 20:38:44 +0700

    [expandableappbarimage] add favorite button

--- lib/component_widgets/expandable_appbar_with_image.dart
+++ lib/component_widgets/expandable_appbar_with_image.dart
@@ -7,11 +7,12 @@ class ExpandableAppbarWithImage extends StatelessWidget {
     super.key,
     required this.maxHeight,
     required this.imgUrl,
-    required this.title,
+    required this.title, required this.icon,
   });
 
   final double maxHeight;
   final String imgUrl, title;
+  final Widget icon;
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +49,18 @@ class ExpandableAppbarWithImage extends StatelessWidget {
           ),
         ),
       ),
+      actions: [
+        ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            padding: EdgeInsets.zero,
+            backgroundColor: Colors.white70,
+            foregroundColor: Colors.white70,
+          ),
+          child: icon,
+        )
+      ],
     );
   }
 }
