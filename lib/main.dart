2eaef45ff9781c822b379b972b19e0a689802309
tataadin <tataa2237@gmail.com>
2024-01-10 21:02:53 +0700

    [main] register new provider preferences [29fa727a2060620262a2527859fd47b622fbd982]

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamental/providers/preferences_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'data/api_service.dart';
 import 'data/models/restaurant.dart';
+import 'helpers/shared_preferences.dart';
 import 'providers/all_restaurant_provider.dart';
 import 'providers/search_restaurant_provider.dart';
 import 'screens/main_screen.dart';
 import 'screens/restaurant/restaurant_page.dart';
 import 'screens/restaurant/restaurant_review/restaurant_review_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -28,34 +32,33 @@ class MyApp extends StatelessWidget {
           create: (_) => SearchRestaurantProvider(
             apiService: ApiService(),
           ),
+        ),
+        ChangeNotifierProvider<PreferencesProvider>(
+          create: (_) => PreferencesProvider(
+            preferencesHelper: PreferencesHelper(
+              sharedPreferences: SharedPreferences.getInstance(),
+            ),
+          ),
         )
       ],
-      child: MaterialApp(
-        title: 'Indonesian Restaurant',
-        theme: ThemeData(
-          useMaterial3: true,
-          textSelectionTheme: const TextSelectionThemeData(
-            cursorColor: Colors.deepOrangeAccent,
-            selectionColor: Colors.deepOrangeAccent,
-            selectionHandleColor: Colors.deepOrangeAccent,
-          ),
-          colorScheme: Theme.of(context).colorScheme.copyWith(
-                primary: Colors.white,
-                onPrimary: Colors.black,
-                secondary: Colors.deepOrangeAccent,
-              ),
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const MainScreen(),
-          '/restaurant': (context) => RestaurantPage(
-                restaurantId:
-                    ModalRoute.of(context)?.settings.arguments as String,
-              ),
-          '/restaurant.review': (context) => RestaurantReviewPage(
-                restaurant:
-                    ModalRoute.of(context)?.settings.arguments as Restaurant,
-              ),
+      child: Consumer<PreferencesProvider>(
+        builder: (context, provider, child) {
+          return MaterialApp(
+            title: 'Indonesian Restaurant',
+            theme: provider.themeData,
+            initialRoute: '/',
+            routes: {
+              '/': (context) => const MainScreen(),
+              '/restaurant': (context) => RestaurantPage(
+                    restaurantId:
+                        ModalRoute.of(context)?.settings.arguments as String,
+                  ),
+              '/restaurant.review': (context) => RestaurantReviewPage(
+                    restaurant: ModalRoute.of(context)?.settings.arguments
+                        as Restaurant,
+                  ),
+            },
+          );
         },
       ),
     );
