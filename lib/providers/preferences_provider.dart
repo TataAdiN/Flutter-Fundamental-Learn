29fa727a2060620262a2527859fd47b622fbd982
tataadin <tataa2237@gmail.com>
2024-01-10 20:20:44 +0700

    [providers] add new provider preferences

---
+++ lib/providers/preferences_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../helpers/shared_preferences.dart';
+import '../themes/styles.dart';
+
+class PreferencesProvider extends ChangeNotifier {
+
+  bool _isDarkTheme = false;
+  bool get isDarkTheme => _isDarkTheme;
+
+  bool _isDailyNewsActive = false;
+  bool get isDailyNewsActive => _isDailyNewsActive;
+
+  PreferencesHelper preferencesHelper;
+
+  PreferencesProvider({required this.preferencesHelper}) {
+    _getTheme();
+    _getDailyNotificationPreferences();
+  }
+
+  ThemeData get themeData => _isDarkTheme ? darkTheme : lightTheme;
+
+  void _getTheme() async {
+    _isDarkTheme = await preferencesHelper.isDarkTheme;
+    notifyListeners();
+  }
+
+  void _getDailyNotificationPreferences() async {
+    _isDailyNewsActive = await preferencesHelper.isDailyNotificationActive;
+    notifyListeners();
+  }
+
+  void enableDarkTheme(bool value) {
+    preferencesHelper.setDarkTheme(value);
+    _getTheme();
+  }
+
+  void enableDailyNotification(bool value) {
+    preferencesHelper.setDailyNotification(value);
+    _getDailyNotificationPreferences();
+  }
+}
