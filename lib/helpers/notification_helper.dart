1ee185c7d4309026f198eded40357c418988ae3e
tataadin <tataa2237@gmail.com>
2024-01-12 21:04:43 +0700

    [helper.notification] format import path

--- lib/helpers/notification_helper.dart
+++ lib/helpers/notification_helper.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:rxdart/rxdart.dart';
 
-import 'package:flutter_fundamental/data/models/restaurant.dart';
+import '../data/models/restaurant.dart';
+
 
 final selectNotificationSubject = BehaviorSubject<String>();
 
